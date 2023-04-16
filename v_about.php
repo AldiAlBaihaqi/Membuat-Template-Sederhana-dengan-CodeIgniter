7f097fa58b93d1ff8404d311a2ca19030f20987c
AldiAlBaihaqi <128796208+AldiAlBaihaqi@users.noreply.github.com>
2023-04-16 16:07:30 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : MUHAMMAD ALDI</li>
+ <li>Nama Belakang : AL BAIHAQI</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> CILEUNGSI KAB.BOGOR</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>JAKARTA</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>BASKET BALL</li>
+ <li>BERENANG</li>
+ </ul>
+</section>
